2d4bd9ecd324b7f6b72bc2f2b5473d479bb5b58c
Muhammad Ihsan Kamil <97449763+Heat41@users.noreply.github.com>
2026-09-18 11:27:13 +0700

    feat: export Stage 2D KTP extraction interfaces

--- autodocscanner/ktp/__init__.py
+++ autodocscanner/ktp/__init__.py
@@ -1,1 +1,23 @@
-"""KTP data and storage domain."""
+"""KTP data, extraction, and storage domain."""
+
+from autodocscanner.ktp.extraction import (
+    KtpExtractionResult,
+    KtpFieldCrop,
+    extract_ktp_regions,
+)
+from autodocscanner.ktp.layout import (
+    KTP_FIELD_BOXES,
+    NormalizedBox,
+    crop_normalized,
+    normalized_to_pixel_box,
+)
+
+__all__ = (
+    "KTP_FIELD_BOXES",
+    "KtpExtractionResult",
+    "KtpFieldCrop",
+    "NormalizedBox",
+    "crop_normalized",
+    "extract_ktp_regions",
+    "normalized_to_pixel_box",
+)
