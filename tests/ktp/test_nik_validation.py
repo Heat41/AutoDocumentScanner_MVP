e4eedf631989ac9471b61683bd0033f305c12bd8
Muhammad Ihsan Kamil <97449763+Heat41@users.noreply.github.com>
2026-09-24 14:55:31 +0700

    Test contextual NIK validation

---
+++ tests/ktp/test_nik_validation.py
@@ -0,0 +1,64 @@
+import unittest
+
+from autodocscanner.ktp.nik_validation import (
+    repair_nik_with_context,
+)
+
+
+class TestNikContextValidation(unittest.TestCase):
+    def test_repairs_province_prefix_when_birth_segment_matches(self):
+        result = repair_nik_with_context(
+            "5112340503800002",
+            province="PROVINSI KALIMANTAN BARAT",
+            birth_date="1980-03-05",
+            gender="LAKI-LAKI",
+        )
+
+        self.assertTrue(
+            result.changed
+        )
+        self.assertTrue(
+            result.context_valid
+        )
+        self.assertEqual(
+            result.value,
+            "6112340503800002",
+        )
+
+    def test_does_not_repair_when_birth_segment_conflicts(self):
+        result = repair_nik_with_context(
+            "5112340101800002",
+            province="KALIMANTAN BARAT",
+            birth_date="1980-03-05",
+            gender="LAKI-LAKI",
+        )
+
+        self.assertFalse(
+            result.changed
+        )
+        self.assertFalse(
+            result.context_valid
+        )
+        self.assertEqual(
+            result.value,
+            "5112340101800002",
+        )
+
+    def test_female_birth_segment_uses_day_plus_forty(self):
+        result = repair_nik_with_context(
+            "6112344503800002",
+            province="KALIMANTAN BARAT",
+            birth_date="1980-03-05",
+            gender="PEREMPUAN",
+        )
+
+        self.assertFalse(
+            result.changed
+        )
+        self.assertTrue(
+            result.context_valid
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
